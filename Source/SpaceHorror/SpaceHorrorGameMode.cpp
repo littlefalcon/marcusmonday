f2aededbcc5af8acfcae2a7380f1f8df1c510436
Tossaporn Ketkwanchai <pointkkc@hotmail.com>
2018-03-07 22:14:33 +0700

    SNL84 add walk sound to player

--- Source/SpaceHorror/SpaceHorrorGameMode.cpp
+++ Source/SpaceHorror/SpaceHorrorGameMode.cpp
@@ -9,7 +9,7 @@ ASpaceHorrorGameMode::ASpaceHorrorGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/DynamicContent/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/StaticContent/FirstPerson/FirstPersonCPP/Blueprints/FirstPersonCharacter")); 
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
